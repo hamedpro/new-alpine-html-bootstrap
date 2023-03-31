415d2b582350a12a6795504e81d31fc9f949c392
hamed yaghoutpour <hamedpro30@gmail.com>
2023-04-01 01:40:46 +0330

    fixing cors of main api router

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import { generator } from "./customRouter1.js";
 import express from "express";
 import { readFileSync } from "fs";
 import { MongoClient, ObjectId } from "mongodb";
+import cors from "cors";
 import jwt from "jsonwebtoken";
 var { api_port, mongodb_url, mongodb_db_name, jwt_secret } = JSON.parse(
 	readFileSync("./env.json", "utf-8")
@@ -9,6 +10,7 @@ var { api_port, mongodb_url, mongodb_db_name, jwt_secret } = JSON.parse(
 var client = new MongoClient(mongodb_url);
 var db = client.db(mongodb_db_name);
 var app = express();
+app.use(cors());
 app.use("/static/assets", express.static("./src/assets"));
 app.use(generator(db));
 
